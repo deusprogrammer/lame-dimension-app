ad4af8a1e0cad741ee81f4495d1d5b7dcc36b099
Michael Main <deusprogrammer@gmail.com>
2024-02-18 16:59:04 -0600

    Added the button and route for database editor

---
+++ lame-dimension-dialogue-tool-react/src/routes/DatabaseEditor.jsx
@@ -0,0 +1,13 @@
+export default () => {
+    return (
+        <div className="container">
+            <div className="left">
+                <h2>Database</h2>
+                
+            </div>
+            <div className="center" style={{ textAlign: 'center' }}>
+                
+            </div>
+        </div>
+    )
+}
